d98420ed263bac527476ff8b801dc11751e6c229
yuanzhicheng <1798788388@qq.com>
2020-05-25 17:00:25 +0800

    3-1完成

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
-let say = "hello";
-console.log(say);
+// let say:string = "hello1";
+// say = 333;
+// console.log(say); 
+function add(a, b) {
+    return a + b;
+}
+let num = add(1, 2);
+let name = 'dsds';
+let age;
+console.log(add(1, 2));
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-let say:string = "hello1";
-console.log(say); 
+// let say:string = "hello1";
+// say = 333;
+// console.log(say); 
+
+function add(a:number,b:number) {
+    return a+b;
+}
+
+let num = add(1,2);
+
+let name = 'dsds';
+let age;
+
+console.log(add(1,2));
