d9e37d4c0ac9d72e59751c5e5f4c72c0c6f821aa
yuanzhicheng <1798788388@qq.com>
2020-05-25 17:44:42 +0800

    3-3完成

--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,46 @@ printValues({
     name:'tom',
     age:33
 })
+
+;
+
+let name:string | undefined; //联合类型name可以是字符串或者undefined
+
+if(typeof name === "string"){
+    //类型保护
+}
+
+
+function printMenu():void{
+    console.log('1,登录');
+    console.log('2,推出');
+}
+
+function throwError(msg:string) : never{
+    throw new Error(msg);
+}
+
+let a:'2';
+
+let gender:"男" | "女";
+gender = "女";
+
+let arr : [];//arr永远只能取值为一个空数组
+
+let user:{
+    name:string,
+    age:number
+}
+
+user = {
+    name:'212',
+    age:12
+}
+
+
+let tu:[string,number];
+tu = ['sds',12];
+
+let data : any = 'dsdsa';
+
+let num:number = data;
