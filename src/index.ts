d69d4fa4215e90dcbed055c8a2110aa33787d249
yuanzhicheng <1798788388@qq.com>
2020-05-25 22:34:22 +0800

    sdfs

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,19 @@
-type Deck = NormalCard[];
-type Color = "♥" | "♠" | "♦" | "♣";
-type NormalCard = {
-    color:Color,
-    mark:number
+enum Gender{
+    Male = "男",
+    Female = "女"
 }
 
-function createDeck():Deck{
-    const deck:Deck = [];
-    for(let i = 1;i<=13;i++){
-        deck.push({
-            mark:i,
-            color:"♠"
-        })
-        deck.push({
-            mark:i,
-            color:"♣"
-        })
-        deck.push({
-            mark:i,
-            color:"♥"
-        })
-        deck.push({
-            mark:i,
-            color:"♦"
-        })
-    }
-    return deck
+let gender:Gender;
+
+gender = Gender.Male;
+gender = Gender.Female
+// console.log(gender);
+
+function printGenders(){
+    const vals = Object.values(Gender);
+    vals.forEach(v => console.log(v));
 }
+printGenders();
+// function searchUsers(g:Gender) {
 
-function printDeck(deck:Deck){
-    let result = "\n";
-    deck.forEach((card,i) => {
-        let str = card.color;
-        if(card.mark <= 10) {
-            str += card.mark;
-        }else if(card.mark === 11) {
-            str += "J";
-        }else if(card.mark === 12) {
-            str += "Q";
-        }else if(card.mark === 13) {
-            str += "K";
-        }
-        result += str + "\t";
-        if((i+1) % 6 === 0) {
-            result += "\n"
-        }
-        
-    })
-    console.log(result);
-}
-
-const deck = createDeck();
-printDeck(deck);
-
+// }
