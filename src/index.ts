817fd13c0a3c6eb62c85ee6f5756f3f4359e7ae7
yuanzhicheng <1798788388@qq.com>
2020-05-26 17:33:31 +0800

    sds

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,70 @@
 
-import { createDeck, printDeck } from "./funcs";
-const deck = createDeck();
-printDeck(deck);
+// interface User {
+//     name:string,
+//     age:number,
+//     sayHello:() => void
+// }
 
+
+// let u:User = {
+//     name:'sdsd',
+//     age:12,
+//     sayHello(){
+//         console.log('sdsds');
+//     }
+// }
+
+// type Condition = {
+//     (n:number):boolean
+// }
+
+interface Condition {
+    (n:number):boolean
+}
+
+function sum(numbers:number[],callBack:Condition){
+    let s = 0;
+    numbers.forEach(n => {
+        if(callBack(n)){
+            s += n;
+        }
+    })
+}
+
+
+
+interface A {
+    t:string
+}
+
+interface B{
+    t2:number
+}
+interface C extends A,B{
+    t3:Boolean
+}
+
+let u:C ={
+    t:'dss',
+    t2:121,
+    t3:true
+}
+
+
+type D = {
+    t:string
+}
+
+type E = {
+    t1:number
+}
+
+type F = {
+    t2:Boolean
+} & D & E
+
+let n:F ={
+    t:'sds',
+    t2:true,
+    t1:121
+}
