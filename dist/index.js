5a62906c3ae48822e4940fc5ae50bf2dc300b15c
yuanzhicheng <1798788388@qq.com>
2020-05-26 12:01:00 +0800

    5-2完成

--- dist/index.js
+++ dist/index.js
@@ -1,48 +1,17 @@
-function isOdd(n) {
-    return n % 2 === 0;
+Object.defineProperty(exports, "__esModule", { value: true });
+const myModule_1 = require("./myModule");
+console.log(myModule_1.name);
+var Permission;
+(function (Permission) {
+    Permission[Permission["Read"] = 1] = "Read";
+    Permission[Permission["Write"] = 2] = "Write";
+    Permission[Permission["Create"] = 4] = "Create";
+    Permission[Permission["Delete"] = 8] = "Delete";
+})(Permission || (Permission = {}));
+let p = Permission.Read | Permission.Write;
+function hasPermission(target, per) {
+    return (target & per) === per;
 }
-// let nums:number[] = [2,3,4];
-let nums = [2, 3, 4];
-let u;
-u = {};
-function printValues(obj) {
-    const vals = Object.values(obj);
-    vals.forEach(v => console.log(v));
-}
-printValues({
-    name: 'tom',
-    age: 33
-});
-let name; //联合类型name可以是字符串或者undefined
-if (typeof name === "string") {
-    //类型保护
-}
-function printMenu() {
-    console.log('1,登录');
-    console.log('2,推出');
-}
-function throwError(msg) {
-    throw new Error(msg);
-}
-let a;
-let gender;
-gender = "女";
-let arr; //arr永远只能取值为一个空数组
-let user;
-user = {
-    name: '212',
-    age: 12
-};
-let tu;
-tu = ['sds', 12];
-let data = 'dsdsa';
-let num = data;
-let p;
-p = {
-    name: 'sdsd',
-    age: 12,
-    gender: '女'
-};
-function getUsers(g) {
-    return [];
-}
+console.log(hasPermission(p, Permission.Write));
+p = p ^ Permission.Write;
+console.log(hasPermission(p, Permission.Write));
---
+++ dist/myModule.js
@@ -0,0 +1,7 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sum = exports.name = void 0;
+exports.name = 'Tom';
+function sum(a, b) {
+    return a + b;
+}
+exports.sum = sum;
