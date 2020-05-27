40edfdb4ae4d322340b22c8e0aa5575b93be50ab
yuanzhicheng <1798788388@qq.com>
2020-05-27 11:31:24 +0800

    7-3访问器

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,30 @@
 class User {
-    constructor(name, age, gender = '男') {
-        this.publishNumber = 3;
-        this.curNumber = 0;
-        this.id = Math.random();
+    constructor(name, _age, gender = '男') {
         this.name = name;
-        this.age = age;
+        this._age = _age;
         this.gender = gender;
+        this._publishNumber = 3;
+        this._curNumber = 0;
+        this.id = Math.random();
+    }
+    set age(value) {
+        if (value < 0) {
+            this._age = 0;
+        }
+        else if (value > 200) {
+            this._age = 200;
+        }
+        else {
+            this._age = Math.floor(value);
+        }
+    }
+    get age() {
+        return this._age;
     }
     publish(title) {
-        if (this.curNumber < this.publishNumber) {
+        if (this._curNumber < this._publishNumber) {
             console.log('发布一篇文章' + title);
-            this.curNumber++;
+            this._curNumber++;
         }
         else {
             console.log('今日发布文章以达到上限');
@@ -18,6 +32,8 @@ class User {
     }
 }
 const u = new User("aa", 2);
+u.age = 15.2;
+console.log(u.age);
 u.publish('文章一');
 u.publish('文章一');
 u.publish('文章一');
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,31 @@ class User {
     readonly id:number //不能改变
     pid?:string
 
-    private publishNumber:number = 3;//每天一共可以发多少篇文章
-    private curNumber:number = 0;//当前可以发布的文章数量
+    private _publishNumber:number = 3;//每天一共可以发多少篇文章
+    private _curNumber:number = 0;//当前可以发布的文章数量
 
-    constructor(public name:string, public age:number,public gender:'男'|'女' = '男'){
+    constructor(public name:string, private _age:number,public gender:'男'|'女' = '男'){
         this.id = Math.random();
     }
 
+    set age(value:number) {
+        if(value < 0) {
+            this._age = 0
+        }else if (value > 200) {
+            this._age = 200
+        }else {
+            this._age = Math.floor(value)
+        }
+    }
+
+    get age(){
+        return this._age
+    }
+
     publish(title:string) {
-        if(this.curNumber < this.publishNumber){
+        if(this._curNumber < this._publishNumber){
             console.log('发布一篇文章' + title);
-            this.curNumber ++
+            this._curNumber ++
         }else{
             console.log('今日发布文章以达到上限')
         }
@@ -20,7 +34,8 @@ class User {
 }
 
 const u = new User("aa",2);
-
+u.age = 15.2;
+console.log(u.age);
 u.publish('文章一');
 u.publish('文章一');
 u.publish('文章一');
