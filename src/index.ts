6b1e840ec3da8f10e2a205868aa815cec4ff939e
yuanzhicheng <1798788388@qq.com>
2020-06-01 11:46:44 +0800

    sd

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,63 @@
-import { Dictionary } from "./dictionary";
+abstract class Chess {
+    x:number = 0
+    y:number = 0
 
-const dic = new Dictionary<string,number>();
+    abstract readonly name: string;
 
-dic.set('a',1)
-dic.set('w',2)
-dic.set('a',3)
-dic.set('c',3)
-dic.forEach((k,v) => {
-    console.log(`${k}:${v}`)
-})
-console.log('当前键值数量为' + dic.size);
-console.log(dic.has("a"))
-dic.delete('c');
-console.log('删除键值对c')
-console.log('当前键值数量为' + dic.size);
-dic.forEach((k,v) => {
-    console.log(`${k}:${v}`)
-})
+    move(targetX:number,targetY:number) : boolean {
+        console.log('1.边界判断');
+        console.log('2.目标位置判断');
+        //3.判断规则
+        if(this.rule(targetX,targetY)) {
+            this.x = targetX;
+            this.y = targetY;
+            console.log(`${this.name}移动成功`)
+            return true;
+        }
+        return false;
+        
+    }
+
+    protected abstract rule(targetX:number,targetY:number):boolean;
+
+    
+}
+
+class Horse extends Chess {
+    protected rule(targetX: number, targetY: number): boolean {
+        return true;
+    }
+   
+    readonly name: string ="马"
+}
+
+class Pao extends Chess {
+    protected rule(targetX: number, targetY: number): boolean {
+        return true;
+    }
+   
+    readonly name:string;
+    constructor () {
+        super();
+        this.name = "炮"
+    }
+
+}
+
+class Solder extends Chess {
+    protected rule(targetX: number, targetY: number): boolean {
+        return true;
+    }
+    
+    get name () {
+        return '兵'
+    }
+}
+
+const h = new Horse();
+const P = new Pao();
+const s = new Solder();
+h.move(2,4);
+P.move(3,5);
+s.move(5,7);
+// const c = new Chess();
