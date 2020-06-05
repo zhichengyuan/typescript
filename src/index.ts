a159873147a2722fe137c126a8303aa2ba6192b3
yuanzhicheng <1798788388@qq.com>
2020-06-05 14:44:00 +0800

    sd

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,14 @@
-function d(target:any,key:string) {
-    console.log(target,key);
-    // console.log(target === A.prototype,key);
-    if(!target.__props){
-        target.__props = []
-    }
-    target.__props.push(key);
+import { classDescriptor, propDescriptor, printObj } from "./Descriptor";
+
+@classDescriptor("用户")
+class User{
+    @propDescriptor('用户')
+    loginId:string
+    @propDescriptor('密码')
+    loginPwd:string
 }
+const u = new User();
+u.loginId = 'sss'
+u.loginPwd = '123'
+printObj(u);
 
-
-function enumerable (target:any,key:string,descriptor:PropertyDescriptor) {
-    descriptor.enumerable = true
-}
-
-function useless(target:any,key:string,descriptor:PropertyDescriptor) {
-    descriptor.value = function () {
-        console.warn(key + '方法已过期');
-    }
-}
-
-
-class A {
-    @d
-    prop1:string
-
-    @d
-    static prop2:string
-
-    @enumerable
-    @useless
-    method1(){
-        console.log('method1');
-    }
-    @enumerable
-    method2(){
-
-    }
-}
-
-// console.log((A.prototype as any).__props);
-
-const a = new A();
-// console.log((a as any).__props);
-for (const key in a) {
-    console.log('===' + key);
-}
-a.method1();
