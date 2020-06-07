8b801dd261f4d4ab131d2d6fed1c0d8e0227303e
yuanzhicheng <1798788388@qq.com>
2020-06-07 17:01:41 +0800

    sds

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,76 @@
-import {printObj, descriptor } from "./Descriptor";
+import "reflect-metadata";
+import { plainToClass} from 'class-transformer'
 
-@descriptor("用户")
-class User{
-    @descriptor('用户')
-    loginId:string
-    @descriptor('密码')
-    loginPwd:string
+class User {
+    id:number
+    firstName:string
+    lastName:string
+    age:number
+
+    getName() {
+        return this.firstName + " " + this.lastName
+    }
+    isAdult() {
+        return this.age > 36 && this.age<60;
+    }
 }
-const u = new User();
-u.loginId = 'sss'
-u.loginPwd = '123'
-printObj(u);
 
-// import "reflect-metadata";
+var arr = [{
+    "id": 1,
+    "firstName": "Johny",
+    "lastName": "Cage",
+    "age": 27
+  },
+  {
+    "id": 2,
+    "firstName": "Ismoil",
+    "lastName": "Somoni",
+    "age": 50
+  },
+  {
+    "id": 3,
+    "firstName": "Luke",
+    "lastName": "Dacascos",
+    "age": 12
+  }]
+ function plain(users:any):any{
+     for(const u of users) {
+         const user = plainToClass(User,u);
+        //  console.log(user.getName(),user.isAdult())
+        
+     }
+ }
 
-// @Reflect.metadata('a',"一个类")
-// @Reflect.metadata('a1',"一个类1")
-// @Reflect.metadata('a2',"一个类2")
-// class A {
-//     @Reflect.metadata('prop','一个属性')
-//     prop1:string
+ plain(arr);
+
+
+
+
+
+// import "reflect-metadata"
+
+// import {IsNotEmpty, validate,MaxLength, MinLength, Min, Max} from 'class-validator'
+
+// class RegUser{
+//     @IsNotEmpty({message:'账号不能为空'})
+//     @MaxLength(4,{message:'账号不能超过4个字符'})
+//     @MinLength(2,{message:"最少不能少于两个字符"})
+//     loginId:string
+//     loginPwd:string
+
+//     @Min(0,{message:"年龄不能少于0"})
+//     @Max(200,{message:"年龄不能大于200岁"})
+//     age:number
+//     gender:"男" | "女"
 // }
 
-// const obj = new A();
-// console.log(Reflect.getMetadata('a',A))
+// const post = new RegUser();
+// post.loginId = '1'
+// post.age = -1
+
+
+// validate(post).then(errors => {
+//     console.log(errors);
+// });
+
+
