fe2ed54ab2a337da6a10f03dfca28995b31571c8
yuanzhicheng <1798788388@qq.com>
2020-06-08 18:37:38 +0800

    sds

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
-import "reflect-metadata"
+// const a = "sdsd";
+// let b:typeof a = "sdsd"
 
-class User {
-    @Reflect.metadata("a","b")
+// class User{
+//     loginId:string
+//     loginpwd:string
+// }
+
+// const u = new User();
+// const u2 = u;
+
+// const A = User;
+
+
+// function createUser(cls:typeof User) : User {
+//     return new cls()
+// }
+
+// const u = createUser(User);
+
+interface User {
     loginId:string
-    @Reflect.metadata("a","b")
+    loginpwd:string
     age:number
-
 }
+
+function printUserProperty(obj:User,prop:keyof User) {
+    console.log(obj[prop])
+}
+
+const u = {
+    loginId:"sasds",
+    loginpwd:"sds",
+    age:34
+}
+
+printUserProperty(u,"age");
